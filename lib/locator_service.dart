58f093aba884aaa2f99c0d2398b5bccf6fa17453
Helohi <helomegido@gmail.com>
2025-03-13 23:19:13 +0300

    locator service update

--- lib/locator_service.dart
+++ lib/locator_service.dart
@@ -1,15 +1,22 @@
 import 'package:get_it/get_it.dart';
+import 'package:{app_name}/feature/presentation/bloc/main_bloc/main_bloc.dart';
+import 'package:{app_name}/feature/data/repositories/data_source_repository_impl.dart';
+
 
 final sl = GetIt.instance;
 
 Future<void> init() async {
   // Bloc
+  sl.registerFactory(() => MainBloc(sl()));
 
   // Usecases
+  // sl.registerFactory(() => usecases.USECASE(repo: sl()));
 
   // Repositories
+  sl.registerLazySingleton<DataSourceRepository>(() => DataSourceRepositoryImpl(dataSource: sl()));
 
   // DataSources
+  // sl.registerLazySingleton<DataSource>(() => DATASOURCEIMPL());
 
   // Core
 
